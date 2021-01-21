fb7bcba8e101ff6d16fed1385108d110468d2fa1
aashwinvaish1 <aashwinvaish1@gmail.com>
2021-01-21 22:54:57 +0530

    Personality touch

--- frontend/src/components/templates/Navbar.js
+++ frontend/src/components/templates/Navbar.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export default class NavBar extends Component {
-    
+
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <div>                
+            <div>
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <Link to="/" className="navbar-brand">Demo</Link>
                     <div className="collapse navbar-collapse">
@@ -28,8 +28,8 @@ export default class NavBar extends Component {
                                 <Link to="/login" className="nav-link">Login</Link>
                             </li>
                             <li className="navbar-item">
-                                <Link to="/profile" className="nav-link">My Profile</Link>
-                            </li>                            
+                                <Link to="/profile" className="nav-link">{localStorage.getItem("email")}</Link>
+                            </li>
                         </ul>
                     </div>
                 </nav>
