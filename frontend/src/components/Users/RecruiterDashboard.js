8afc2d6f0eb2eae5f97cf17655a81babef58c368
aashwinvaish1 <aashwinvaish1@gmail.com>
2021-01-21 22:55:21 +0530

    Recruiter Can Edit and delete jobs now

--- frontend/src/components/Users/RecruiterDashboard.js
+++ frontend/src/components/Users/RecruiterDashboard.js
@@ -15,6 +15,9 @@ import Divider from '@material-ui/core/Divider';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import DoneIcon from '@material-ui/icons/Done';
 
 import SearchIcon from "@material-ui/icons/Search";
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
@@ -27,16 +30,58 @@ class RecruiterDashboard extends Component {
         super(props);
         this.state = {
             jobs: [],
-            salarySortedJobs: [],
-            durationSortedJobs: [],
-            ratingSortedJobs: [],
-            sortBy: "none",
-            sortAsc: true
+            editing: 'none',
+            newMaxApplications: '',
+            newMaxPositions: '',
+            newDeadline: '',
         };
-        this.sortedIcon = this.sortedIcon.bind(this);
-        this.sortBySalary = this.sortBySalary.bind(this);
-        this.sortByDuration = this.sortByDuration.bind(this);
-        this.sortByRating = this.sortByRating.bind(this);
+
+        this.onEditJob = this.onEditJob.bind(this);
+        this.onDeleteJob = this.onDeleteJob.bind(this);
+        this.onChangeValue = this.onChangeValue.bind(this);
+
+
+    }
+
+    onChangeValue(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    onEditJob(id, index) {
+        console.log(id);
+        const newLimit = {
+            applications: this.state.newMaxApplications,
+            positions: this.state.newMaxPositions
+        }
+        const newDeadline = this.state.newDeadline;
+        axios.post('http://localhost:4000/job/' + id + '/edit', {
+            email: localStorage.getItem("email"),
+            password: localStorage.getItem("password"),
+            limit: newLimit,
+            deadline: newDeadline,
+        }).then(response => {
+            console.log(response.data.error);
+            var jobs = [...this.state.jobs];
+            jobs[index].limit = newLimit;
+            jobs[index].deadline = newDeadline;
+            this.setState({ jobs: jobs, editing: 'none' });
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    onDeleteJob(id, index) {
+        console.log(id);
+        axios.delete('http://localhost:4000/job/' + id, { data: { email: localStorage.getItem("email"), password: localStorage.getItem("password") } })
+            .then(response => {
+                console.log(response.data.error);
+                var jobs = [...this.state.jobs];
+                jobs.splice(index, 1);
+                this.setState({ jobs: jobs });
+            })
+            .catch(error => {
+                console.log(error.data.error);
+            })
     }
 
     componentDidMount() {
@@ -54,165 +99,104 @@ class RecruiterDashboard extends Component {
             })
     }
 
-    sortBySalary() {
-        /**
-         *      Note that this is sorting only at front-end.
-         */
-        var array = this.state.jobs;
-        var flag = this.state.sortAsc;
-        array.sort(function (a, b) {
-            if (a.salary != undefined && b.salary != undefined) {
-                return (1 - flag * 2) * (a.salary - b.salary);
-            }
-            else {
-                return 1;
-            }
-        });
-        this.setState({
-            jobs: array,
-            sortAsc: !this.state.sortAsc,
-            sortBy: "salary"
-        })
-    }
-    sortByDuration() {
-        /**
-         *      Note that this is sorting only at front-end.
-         */
-        var array = this.state.jobs;
-        var flag = this.state.sortAsc;
-        array.sort(function (a, b) {
-            if (a.duration != undefined && b.duration != undefined) {
-                return (1 - flag * 2) * (a.duration - b.duration);
-            }
-            else {
-                return 1;
-            }
-        });
-        this.setState({
-            jobs: array,
-            sortAsc: !this.state.sortAsc,
-            sortBy: "duration"
-        })
-    }
-    sortByRating() {
-        /**
-         *      Note that this is sorting only at front-end.
-         */
-        var array = this.state.jobs;
-        var flag = this.state.sortAsc;
-        array.sort(function (a, b) {
-            if (a.rating != undefined && b.rating != undefined) {
-                return (1 - flag * 2) * (a.rating - b.rating);
-            }
-            else {
-                return 1;
-            }
-        });
-        this.setState({
-            jobs: array,
-            sortAsc: !this.state.sortAsc,
-            sortBy: "rating"
-        })
-    }
-
-    sortedIcon(field) {
-        if (field == this.state.sortBy) {
-            if (this.state.sortAsc) {
-                return (
-                    <ArrowDownwardIcon />
-                )
-            }
-            else {
-                return (
-                    <ArrowUpwardIcon />
-                )
-            }
-        }
-    }
-
     render() {
         return (
             <div>
-                <Grid container>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <List component="nav" aria-label="mailbox folders">
-                            <ListItem>
-                                <h3>Filters</h3>
-                            </ListItem>
-                        </List>
-                    </Grid>
-                    <Grid item xs={12} md={9} lg={9}>
-                        <List component="nav" aria-label="mailbox folders">
-                            <TextField
-                                id="standard-basic"
-                                label="Search"
-                                fullWidth={true}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment>
-                                            <IconButton>
-                                                <SearchIcon />
-                                            </IconButton>
-                                        </InputAdornment>
-                                    )
-                                }}
-                            />
-                        </List>
-                    </Grid>
-                </Grid>
-                <Grid container>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <List component="nav" aria-label="mailbox folders">
-
-                            <ListItem button>
-                                <form noValidate autoComplete="off">
-                                    <label>Salary</label>
-                                    <TextField id="standard-basic" label="Enter Min" fullWidth={true} />
-                                    <TextField id="standard-basic" label="Enter Max" fullWidth={true} />
-                                </form>
-                            </ListItem>
-                            <Divider />
-                            <ListItem button divider>
-                                <Autocomplete
-                                    id="combo-box-demo"
-                                    options={this.state.jobs}
-                                    getOptionLabel={(option) => option.name}
-                                    style={{ width: 300 }}
-                                    renderInput={(params) => <TextField {...params} label="Select Names" variant="outlined" />}
-                                />
-                            </ListItem>
-                        </List>
-                    </Grid>
-                    <Grid item xs={12} md={9} lg={9}>
-                        <Paper>
-                            <Table size="small">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Title</TableCell>
-                                        <TableCell>Recruiter Name</TableCell>
-                                        <TableCell><Button onClick={this.sortByDuration}>Duration</Button>{this.sortedIcon("duration")}</TableCell>
-                                        <TableCell><Button onClick={this.sortBySalary}>Salary</Button>{this.sortedIcon("salary")}</TableCell>
-                                        <TableCell>Deadline</TableCell>
-                                        <TableCell><Button onClick={this.sortByRating}>Rating</Button>{this.sortedIcon("rating")}</TableCell>
+                <Grid item xs={12}>
+                    <Paper>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Title</TableCell>
+                                    <TableCell>Date of posting</TableCell>
+                                    <TableCell>Applications (Submitted/Max)</TableCell>
+                                    <TableCell>Number of positions</TableCell>
+                                    <TableCell>Deadline</TableCell>
+                                    <TableCell>Actions</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {this.state.jobs.map((job, ind) => (
+                                    <TableRow key={ind}>
+                                        <TableCell>{job.title}</TableCell>
+                                        <TableCell>{new Date(job.postedOn).toLocaleDateString()}</TableCell>
+                                        <TableCell>
+                                            {job.count.applications}/
+                                            {(this.state.editing == ind) ?
+                                                <TextField
+                                                    size="small"
+                                                    variant="outlined"
+                                                    className="form-control"
+                                                    value={this.state.newMaxApplications}
+                                                    onChange={this.onChangeValue}
+                                                    name="newMaxApplications"
+                                                    type="number"
+                                                    required
+                                                />
+                                                :
+                                                job.limit.applications
+                                            }
+                                        </TableCell>
+                                        <TableCell>
+                                            {(this.state.editing == ind) ?
+                                                <TextField
+                                                    variant="outlined"
+                                                    size="small"
+                                                    className="form-control"
+                                                    value={this.state.newMaxPositions}
+                                                    onChange={this.onChangeValue}
+                                                    name="newMaxPositions"
+                                                    type="number"
+                                                    required
+                                                />
+                                                :
+                                                job.limit.positions
+                                            }
+                                        </TableCell>
+                                        <TableCell>
+                                            {(this.state.editing == ind) ?
+                                                <TextField
+                                                    variant="outlined"
+                                                    size="small"
+                                                    className="form-control"
+                                                    value={this.state.newDeadline.toString().replace('Z', '')}
+                                                    onChange={this.onChangeValue}
+                                                    name="newDeadline"
+                                                    type="datetime-local"
+                                                    required
+                                                />
+                                                :
+                                                new Date(job.deadline).toLocaleString()
+                                            }
+                                        </TableCell>
+                                        <TableCell>
+                                            {(this.state.editing == ind) ?
+                                                <Button color="primary" onClick={this.onEditJob.bind(this, job._id, ind)}>
+                                                    <DoneIcon></DoneIcon>
+                                                </Button>
+                                                :
+                                                <Button color="primary" onClick={() => {
+                                                    this.setState({
+                                                        editing: ind,
+                                                        newDeadline: job.deadline,
+                                                        newMaxPositions: job.limit.positions,
+                                                        newMaxApplications: job.limit.applications
+                                                    })
+                                                }}>
+                                                    <EditIcon></EditIcon>
+                                                </Button>
+                                            }
+                                            <Button color="secondary" onClick={this.onDeleteJob.bind(this, job._id, ind)}>
+                                                <DeleteForeverIcon></DeleteForeverIcon>
+                                            </Button>
+                                        </TableCell>
                                     </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {this.state.jobs.map((job, ind) => (
-                                        <TableRow key={ind}>
-                                            <TableCell>{job.title}</TableCell>
-                                            <TableCell>{job.recruitername}</TableCell>
-                                            <TableCell>{job.duration}</TableCell>
-                                            <TableCell>{job.salary}</TableCell>
-                                            <TableCell>{job.deadline}</TableCell>
-                                            <TableCell>{job.rating}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </Paper>
-                    </Grid>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Paper>
                 </Grid>
-            </div>
+            </div >
         )
     }
 }
