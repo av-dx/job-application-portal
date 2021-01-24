c411f9ebeccff7577c6e0137886ca10d8ead834a
aashwinvaish1 <aashwinvaish1@gmail.com>
2021-01-24 18:07:24 +0530

    Corrected bio validation

--- backend/models/Recruiter.js
+++ backend/models/Recruiter.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 // Create Schema
 const RecruiterSchema = new Schema({
   name: {type: String, required: true, minlength: 1},
-  email: {type: String, required: true, unique: true, match: /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/},
+  email: {type: String, required: true, unique: true, match: /^[\w\-]+(\.[\w\-]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/},
   contact: {type: String, required: true, match: /^[\+]?[0-9]{10,12}$/},
-  bio: {type: String, required: false, match: /^([a-zA-Z]+[\. ]+){0, 250}/},
+  bio: {type: String, required: false, match: /^\s*\S+(?:\s+\S+){0,249}\s*$/},
   password: {type: String, required: true},
   date: {type: Date, required: false},
   jobs: [{type: Schema.Types.ObjectId, ref: 'Jobs'}],
